8c195c02aeba77f4aaaf1aae64abedc2fbfb3521
RB664 <ridhaabenjamin06@gmail.com>
2022-06-17 11:41:55 +0200

    first commit

---
+++ script/code.js
@@ -0,0 +1,10 @@
+document.getElementById('result-weight');
+document.getElementById('result-height');
+document.getElementById('result-results');
+
+
+function calculateBMI() {
+    result.value3 = weight.value1 / (height.value2 * height.value2)
+}
+
+document.write(value3)
